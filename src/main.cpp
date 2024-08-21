f10c039aafcd7d0840000fefa2af8d832208a0d1
sheepdotcom <magicsheepwizard3@gmail.com>
2024-08-20 20:45:14 -0400

    cool :3

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,22 @@ using namespace geode::prelude;
 
 #include <Geode/modify/MenuLayer.hpp>
 class $modify(HollowMenu, MenuLayer) {
-	static void onModify(auto & self) {
+	struct Fields {
+		float m_textGapDist = 40.f;
+		float m_textScale = 0.5f;
+	};
+
+	static void onModify(auto& self) {
 		self.setHookPriority("MenuLayer::init", INT_MIN/2 - 100);
+	}
+
+	CCMenuItemSpriteExtra* createTextButton(std::string name, std::string id, CCPoint pos, SEL_MenuHandler callback) {
+		auto text = CCLabelBMFont::create(name.c_str(), "bigFont.fnt");
+		text->setScale(m_fields->m_textScale);
+		auto btn = CCMenuItemSpriteExtra::create(text, this, callback);
+		btn->setPosition(pos);
+		btn->setID(id);
+		return btn;
 	}
 
 	bool init() {
@@ -19,39 +33,33 @@ class $modify(HollowMenu, MenuLayer) {
 		}
 
 		auto hollowMenu = CCMenu::create();
+		hollowMenu->setZOrder(59);
 		hollowMenu->setID("hollow-menu"_spr);
 		hollowMenu->ignoreAnchorPointForPosition(false);
 		this->addChild(hollowMenu);
 
-		auto mainButtonsMenus = CCMenu::create();
-		mainButtonsMenus->setID("main-buttons-menu");
-		mainButtonsMenus->ignoreAnchorPointForPosition(false);
-		hollowMenu->addChild(mainButtonsMenus);
+		auto mainButtonsMenu = CCMenu::create();
+		mainButtonsMenu->setID("main-buttons-menu");
+		mainButtonsMenu->ignoreAnchorPointForPosition(false);
+		hollowMenu->addChild(mainButtonsMenu);
 
 		auto modSpr = CCSprite::createWithSpriteFrameName("GJ_optionsBtn_001.png");
 		modSpr->setScale(0.7f);
 		auto modBtn = CCMenuItemSpriteExtra::create(modSpr, this, menu_selector(HollowMenu::onMods));
 		modBtn->setPosition(ccp(winSize.width - 30.f, 30.f));
 		modBtn->setID("geode-mod-button");
-		mainButtonsMenus->addChild(modBtn);
+		hollowMenu->addChild(modBtn);
 
-		auto playText = CCLabelBMFont::create("Play", "bigFont.fnt");
-		auto playBtn = CCMenuItemSpriteExtra::create(playText, this, menu_selector(MenuLayer::onPlay));
-		playBtn->setPosition(winSize / 2 + ccp(0.f, 20.f));
-		playBtn->setID("play-button");
-		mainButtonsMenus->addChild(playBtn);
-
-		auto editorText = CCLabelBMFont::create("Editor", "bigFont.fnt");
-		auto editorBtn = CCMenuItemSpriteExtra::create(editorText, this, menu_selector(MenuLayer::onCreator));
-		editorBtn->setPosition(winSize / 2 - ccp(0.f, 20.f));
-		editorBtn->setID("editor-button");
-		mainButtonsMenus->addChild(editorBtn);
-
-		auto quitText = CCLabelBMFont::create("Quit", "bigFont.fnt");
-		auto quitBtn = CCMenuItemSpriteExtra::create(quitText, this, menu_selector(MenuLayer::onQuit));
-		quitBtn->setPosition(winSize / 2 - ccp(0.f, 60.f));
-		quitBtn->setID("quit-button");
-		mainButtonsMenus->addChild(quitBtn);
+		auto playBtn = createTextButton("Play", "play-button", ccp(winSize.width/2, winSize.height/2 + 0.f), menu_selector(MenuLayer::onPlay));
+		auto editBtn = createTextButton("Editor", "editor-button", playBtn->getPosition() - ccp(0.f, m_fields->m_textGapDist * m_fields->m_textScale), menu_selector(MenuLayer::onCreator));
+		auto optBtn = createTextButton("Options", "options-button", editBtn->getPosition() - ccp(0.f, m_fields->m_textGapDist * m_fields->m_textScale), menu_selector(MenuLayer::onOptions));
+		auto achBtn = createTextButton("Achievements", "achievements-button", optBtn->getPosition() - ccp(0.f, m_fields->m_textGapDist * m_fields->m_textScale), menu_selector(MenuLayer::onAchievements));
+		auto exitBtn = createTextButton("Quit", "exit-button", achBtn->getPosition() - ccp(0.f, m_fields->m_textGapDist * m_fields->m_textScale), menu_selector(MenuLayer::onQuit));
+		mainButtonsMenu->addChild(playBtn);
+		mainButtonsMenu->addChild(editBtn);
+		mainButtonsMenu->addChild(optBtn);
+		mainButtonsMenu->addChild(achBtn);
+		mainButtonsMenu->addChild(exitBtn);
 
 		return true;
 	}
